824c401696db903c8279d116ae0c1dd6105ca0d8
Albertsch <albertsch@posteo.de>
2024-06-21 19:20:39 +0200

    simple first implementation of a edge function
    is used in this commit to rasterize a simple red triangle
    the coordinate in screen are given precomputed. z-buffers also need to be implemented

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
+// Vectors are generally represented as an array with a length equal to the dimension
+
+// Function to determine wheter a pixel is on a specific side of an edge
+float edgefunc(int vertex[2], int corner[2], float pixel[2]);
+
+// The precalculated relative position of the surrounding points of the triangle on the screen
+const float vertices[3][2] = {
+	{0.25, 0.25},
+	{0.50, 0.75},
+	{0.75, 0.25},
+};
+
 int main(int argc, char * argv[]) {
 	// The dimensions of the image
 	int imagewidth = 800;
@@ -10,6 +22,46 @@ int main(int argc, char * argv[]) {
 	// A buffer large enough to hold all the pixel data
 	uint32_t * fb = malloc(sizeof(uint32_t)*imagewidth*imageheight);
 	memset(fb, 0x0, sizeof(uint32_t)*imagewidth*imageheight);
+
+	// For holding the absolute positions of the points on screen
+	int verts[3][2];
+	// Containing the three edges of the triangle
+	for (int i = 0; i < 3; i++) {
+		verts[i][0] = (int) (vertices[i][0] * imagewidth);
+		verts[i][1] = (int) (vertices[i][1] * imageheight);
+	}
+
+	// In order to build up a bounding box of the triangle
+	int minx = INT32_MAX, miny = INT32_MAX, maxx = 0, maxy = 0;
+	for (int i = 0; i < 3; i++) {
+		minx = verts[i][0] < minx ? verts[i][0] : minx;
+		miny = verts[i][1] < miny ? verts[i][1] : miny;
+		maxx = verts[i][0] > maxx ? verts[i][0] : maxx;
+		maxy = verts[i][1] > maxy ? verts[i][1] : maxy;
+	}
+	if (minx < 0) minx = 0;
+	if (miny < 0) miny = 0;
+	if (maxx > imagewidth) maxx = imagewidth;
+	if (maxy > imageheight) maxy = imageheight;
+	// Rendering of the triangle
+	for (int y = miny; y < maxy; y++) {
+		for (int x = minx; x < maxx; x++) {
+			float pixel[2];
+			pixel[0] = x + 0.5;
+			pixel[1] = y + 0.5;
+			// Calculating the determinants which basically just checks whether
+			// the pixel is to a side of the edge
+			float det0 = edgefunc(verts[0], verts[1], pixel);
+			float det1 = edgefunc(verts[1], verts[2], pixel);
+			float det2 = edgefunc(verts[2], verts[0], pixel);
+			// Combining all determinants it is checked whether all determinants are
+			// greater or equal than 0. If so the color is set to red
+			if (det0 >= 0.0 && det1 >= 0.0 && det2 >= 0.0) {
+				fb[y * imagewidth + x] = 0x00ff0000;
+			}
+		}
+	}
+
 	printf("P3\n%d %d\n255\n", imagewidth, imageheight);
 	for (size_t y = imageheight; y > 0; y--) {
 		for (size_t x = 0; x < imagewidth; x++) {
@@ -21,4 +73,8 @@ int main(int argc, char * argv[]) {
 		}
 	}
 	return 0;
+}
+
+float edgefunc(int vertex[2], int corner[2], float pixel[2]) {
+	return (pixel[0] - vertex[0]) * (corner[1] - vertex[1]) - (pixel[1] - vertex[1]) * (corner[0] - vertex[0]);
 }
